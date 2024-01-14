0364c1f103d6d9b7f48e096a8c2bf2df491c8edb
Muhamad Fathul Azis <azisfathl@gmail.com>
2024-01-14 15:30:38 +0700

    add localization provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:dicoding_subscriptions/common.dart';
 import 'package:dicoding_subscriptions/home.dart';
+import 'package:dicoding_subscriptions/localizations_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,24 +13,31 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Localization & Accessibility',
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
-        ),
-        scaffoldBackgroundColor: Colors.grey.shade50,
-        outlinedButtonTheme: OutlinedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.blueGrey.shade800,
-            foregroundColor: Colors.white,
+    return ChangeNotifierProvider(
+      create: (context) => LocalizationProvider(),
+      builder: (context, child) {
+        final provider = Provider.of<LocalizationProvider>(context);
+        return MaterialApp(
+          locale: provider.locale,
+          title: 'Flutter Localization & Accessibility',
+          theme: ThemeData(
+            appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.white,
+              foregroundColor: Colors.black,
+            ),
+            scaffoldBackgroundColor: Colors.grey.shade50,
+            outlinedButtonTheme: OutlinedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blueGrey.shade800,
+                foregroundColor: Colors.white,
+              ),
+            ),
           ),
-        ),
-      ),
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: const HomePage(),
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          home: const HomePage(),
+        );
+      },
     );
   }
 }
